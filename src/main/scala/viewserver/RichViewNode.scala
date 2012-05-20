0619e84a6444e193962ef9d31ef6f7311c23d437
Carl Harroch <carl@novoda.com>
2012-05-20 12:34:59 +0100

    added traversable

--- src/main/scala/viewserver/RichViewNode.scala
+++ src/main/scala/viewserver/RichViewNode.scala
@@ -6,11 +6,21 @@ import com.android.hierarchyviewerlib.device.ViewNode.Property
 import collection.JavaConversions._
 import collection.TraversableLike
 
-class RichViewNode(node: ViewNode) extends TraversableLike {
+class RichViewNode(node: ViewNode) extends Traversable[ViewNode] {
+
+  def foreach[U](f: ViewNode => U) = views(node).foreach(f)
 
   def text: Option[String] = node.properties.find(isPropTextView) match {
     case Some(prop) => Some(prop.value)
     case _ => None
+  }
+
+  def views(f: ViewNode): Stream[ViewNode] = {
+    if (f.children.isEmpty) {
+      Stream(f)
+    } else {
+      f.children.toStream.map(views).flatten
+    }
   }
 
   def isTextView = node.properties.exists(isPropTextView)
